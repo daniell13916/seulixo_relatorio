d7de33589b5c59d474cedcc489de1195045fab8e
daniell13916 <152323615+daniell13916@users.noreply.github.com>
2024-06-17 17:51:20 -0300

    Update relatorio_seulixoo.py

--- relatorio_seulixoo.py
+++ relatorio_seulixoo.py
@@ -384,122 +384,122 @@ def generate_report(senha_empresa, data_inicio, data_fim):
         password="postgres"
     )
 
-        # Abrir um cursor para executar consultas SQL
-        with conn.cursor() as cur:
-            # Consulta SQL para obter informações da empresa com base na senha fornecida
-            cur.execute("SELECT id, empresa FROM public.users WHERE password = %s;", (senha_empresa,))
-            empresa_info = cur.fetchone()
+    # Abrir um cursor para executar consultas SQL
+    with conn.cursor() as cur:
+        # Consulta SQL para obter informações da empresa com base na senha fornecida
+        cur.execute("SELECT id, empresa FROM public.users WHERE password = %s;", (senha_empresa,))
+        empresa_info = cur.fetchone()
 
-            if empresa_info:
-                user_id, empresa = empresa_info  # Definindo a variável empresa aqui
+        if empresa_info:
+            user_id, empresa = empresa_info  # Definindo a variável empresa aqui
 
-                # Consulta SQL para obter a porcentagem de rejeitos com base na senha fornecida
-                cur.execute("""
-                    SELECT porcentagem_rejeitos
-                    FROM users
-                    WHERE password = %s;
-                """, (senha_empresa,))
-                porcentagem_rejeitos = cur.fetchone()
+            # Consulta SQL para obter a porcentagem de rejeitos com base na senha fornecida
+            cur.execute("""
+                SELECT porcentagem_rejeitos
+                FROM users
+                WHERE password = %s;
+            """, (senha_empresa,))
+            porcentagem_rejeitos = cur.fetchone()
 
-                if porcentagem_rejeitos is not None:
-                    porcentagem_rejeitos = float(porcentagem_rejeitos[0])  # Converter para float
+            if porcentagem_rejeitos is not None:
+                porcentagem_rejeitos = float(porcentagem_rejeitos[0])  # Converter para float
 
-                    # Consulta SQL para obter os dados de coleta da empresa no período especificado
-                    cur.execute(f"""
-                        SELECT data, volume
-                        FROM "Dados de coleta".{empresa}
-                        WHERE data >= %s AND data <= %s;
-                    """, (data_inicio, data_fim))
-                    coleta_data = cur.fetchall()
+                # Consulta SQL para obter os dados de coleta da empresa no período especificado
+                cur.execute(f"""
+                    SELECT data, volume
+                    FROM "Dados de coleta".{empresa}
+                    WHERE data >= %s AND data <= %s;
+                """, (data_inicio, data_fim))
+                coleta_data = cur.fetchall()
 
-                    if coleta_data:
-                        # Cálculo do total de coletas e volume coletado
-                        total_coletas = len(coleta_data)
-                        total_volume_coletado = sum(float(row[1]) for row in coleta_data)  # Convertendo para float
-                        perda_rejeito = total_volume_coletado * (porcentagem_rejeitos / 100)
-                        volume_destinado_corretamente = total_volume_coletado - perda_rejeito
+                if coleta_data:
+                    # Cálculo do total de coletas e volume coletado
+                    total_coletas = len(coleta_data)
+                    total_volume_coletado = sum(float(row[1]) for row in coleta_data)  # Convertendo para float
+                    perda_rejeito = total_volume_coletado * (porcentagem_rejeitos / 100)
+                    volume_destinado_corretamente = total_volume_coletado - perda_rejeito
 
-                        # Formatação da data do relatório
-                        data_relatorio = time.strftime("%d de %B de %Y")
+                    # Formatação da data do relatório
+                    data_relatorio = time.strftime("%d de %B de %Y")
 
-                        # Formatação das datas de início e fim
-                        data_inicio_formatada = data_inicio.strftime("%d/%m/%Y")
-                        data_fim_formatada = data_fim.strftime("%d/%m/%Y")
+                    # Formatação das datas de início e fim
+                    data_inicio_formatada = data_inicio.strftime("%d/%m/%Y")
+                    data_fim_formatada = data_fim.strftime("%d/%m/%Y")
 
-                        # Escrita do relatório
-                        st.markdown("<h1 style='color: #38b6ff;'>Relatório de Coleta</h1>", unsafe_allow_html=True)
-                        st.write("Plano de Gerenciamento de Resíduos Sólidos (PGRS)")
-                        st.write(f"Uberlândia, {data_relatorio}")
-                        st.write(f"No período entre {data_inicio_formatada} a {data_fim_formatada} foram feitas {total_coletas} coletas, totalizando cerca de {round(total_volume_coletado, 2)} kg coletados.")
-                        st.write(f"Foi considerada uma perda de {porcentagem_rejeitos}% de rejeito ou materiais não recicláveis nos recipientes de coleta.")
-                        st.write(f"Ao final do período conseguimos destinar corretamente {round(volume_destinado_corretamente, 2)} kg, reinserindo-os na economia circular, através da reciclagem e da compostagem.")
-                        st.markdown("<h2 style='color: #38b6ff;'>Análise Gravimétrica</h2>", unsafe_allow_html=True)
-                        st.write("Porcentagem de cada tipo de material em relação ao peso total")
+                    # Escrita do relatório
+                    st.markdown("<h1 style='color: #38b6ff;'>Relatório de Coleta</h1>", unsafe_allow_html=True)
+                    st.write("Plano de Gerenciamento de Resíduos Sólidos (PGRS)")
+                    st.write(f"Uberlândia, {data_relatorio}")
+                    st.write(f"No período entre {data_inicio_formatada} a {data_fim_formatada} foram feitas {total_coletas} coletas, totalizando cerca de {round(total_volume_coletado, 2)} kg coletados.")
+                    st.write(f"Foi considerada uma perda de {porcentagem_rejeitos}% de rejeito ou materiais não recicláveis nos recipientes de coleta.")
+                    st.write(f"Ao final do período conseguimos destinar corretamente {round(volume_destinado_corretamente, 2)} kg, reinserindo-os na economia circular, através da reciclagem e da compostagem.")
+                    st.markdown("<h2 style='color: #38b6ff;'>Análise Gravimétrica</h2>", unsafe_allow_html=True)
+                    st.write("Porcentagem de cada tipo de material em relação ao peso total")
 
-                        # Chamar a função para buscar os valores das colunas e criar o gráfico
-                        buscar_valores_e_criar_grafico(senha_empresa)
+                    # Chamar a função para buscar os valores das colunas e criar o gráfico
+                    buscar_valores_e_criar_grafico(senha_empresa)
 
-                        # Calcular economias com base nas proporções
-                        proporcoes = solicitar_proporcoes(senha_empresa)
-                        if proporcoes:
-                            resultado = calcular_economias(*proporcoes, volume_destinado_corretamente)
+                    # Calcular economias com base nas proporções
+                    proporcoes = solicitar_proporcoes(senha_empresa)
+                    if proporcoes:
+                        resultado = calcular_economias(*proporcoes, volume_destinado_corretamente)
 
-                            # Exibir resultados das economias
-                            st.markdown("<h2 style='color: #38b6ff;'>Ganhos Ambientais</h2>", unsafe_allow_html=True)
-                            st.write("Dados dos ganhos ambientais na preservação do meio ambiente alcançados com a destinação correta dos resíduos recicláveis e orgânicos.")
+                        # Exibir resultados das economias
+                        st.markdown("<h2 style='color: #38b6ff;'>Ganhos Ambientais</h2>", unsafe_allow_html=True)
+                        st.write("Dados dos ganhos ambientais na preservação do meio ambiente alcançados com a destinação correta dos resíduos recicláveis e orgânicos.")
 
-                            # Dividindo os resultados em uma matriz 3x2
-                            num_rows = 3
-                            num_cols = 2
-                            resultados = list(resultado.items())
+                        # Dividindo os resultados em uma matriz 3x2
+                        num_rows = 3
+                        num_cols = 2
+                        resultados = list(resultado.items())
 
-                            # Dicionário de emojis correspondentes aos diferentes tipos de economias
-                            emojis = {
-                                "Economia de Energia (kWh)": "💡",
-                                "Economia de Água (litros)": "💧",
-                                "Redução de CO2 (kg)": "🌍",
-                                "Redução de Volume no Aterro (litros)": "♻️",
-                                "Economia de Árvores (%)": "🌳",
-                                "Economia de Petróleo (litros)": "⛽"
+                        # Dicionário de emojis correspondentes aos diferentes tipos de economias
+                        emojis = {
+                            "Economia de Energia (kWh)": "💡",
+                            "Economia de Água (litros)": "💧",
+                            "Redução de CO2 (kg)": "🌍",
+                            "Redução de Volume no Aterro (litros)": "♻️",
+                            "Economia de Árvores (%)": "🌳",
+                            "Economia de Petróleo (litros)": "⛽"
+                        }
+
+                        for i in range(num_rows):
+                            for j in range(num_cols):
+                                index = i * num_cols + j
+                                if index < len(resultados):
+                                    chave, valor = resultados[index]
+                                    # Adicionar emoji correspondente à economia
+                                    emoji = emojis.get(chave, "")
+                                    # Criar a moldura com o emoji e o valor
+                                    st.markdown(f"<div style='border: 1px solid black; padding: 20px; text-align: center; color: #38b6ff;'>{emoji} {chave}: {valor}</div>", unsafe_allow_html=True)
+                                else:
+                                    # Criar uma moldura vazia
+                                    st.markdown("<div style='border: 1px solid black; padding: 20px;'></div>", unsafe_allow_html=True)
+
+                        # Colorindo os títulos em azul
+                        st.markdown(
+                            """
+                            <style>
+                            .title-text {
+                                color: #38b6ff;
                             }
+                            </style>
+                            """,
+                            unsafe_allow_html=True
+                        )
 
-                            for i in range(num_rows):
-                                for j in range(num_cols):
-                                    index = i * num_cols + j
-                                    if index < len(resultados):
-                                        chave, valor = resultados[index]
-                                        # Adicionar emoji correspondente à economia
-                                        emoji = emojis.get(chave, "")
-                                        # Criar a moldura com o emoji e o valor
-                                        st.markdown(f"<div style='border: 1px solid black; padding: 20px; text-align: center; color: #38b6ff;'>{emoji} {chave}: {valor}</div>", unsafe_allow_html=True)
-                                    else:
-                                        # Criar uma moldura vazia
-                                        st.markdown("<div style='border: 1px solid black; padding: 20px;'></div>", unsafe_allow_html=True)
-
-                            # Colorindo os títulos em azul
-                            st.markdown(
-                                """
-                                <style>
-                                .title-text {
-                                    color: #38b6ff;
-                                }
-                                </style>
-                                """,
-                                unsafe_allow_html=True
-                            )
-
-                            st.markdown("<h2 style='color: #38b6ff;'>Gabriela Brant</h2>", unsafe_allow_html=True)
-                            st.write("Responsável Técnica Seu Lixo LTDA")
-                            st.markdown("<h2 style='color: #38b6ff;'>Alexandre Corrêa</h2>", unsafe_allow_html=True)
-                            st.write("Diretor Seu Lixo LTDA")
-                    else:
-                        st.error("Não há dados de coleta para o período especificado.")
-            else:
-                st.error("Senha da empresa não encontrada.")
-    except TypeError:
-        st.error("Dados sobre as proporções de resíduos auxentes. Peça para o moderador fazer uma avaliação ou inserir os dados após a análise.")
-    except psycopg2.Error as e:
-        st.error(f"Erro ao conectar no banco de dados.")
+                        st.markdown("<h2 style='color: #38b6ff;'>Gabriela Brant</h2>", unsafe_allow_html=True)
+                        st.write("Responsável Técnica Seu Lixo LTDA")
+                        st.markdown("<h2 style='color: #38b6ff;'>Alexandre Corrêa</h2>", unsafe_allow_html=True)
+                        st.write("Diretor Seu Lixo LTDA")
+                else:
+                    st.error("Não há dados de coleta para o período especificado.")
+        else:
+            st.error("Senha da empresa não encontrada.")
+except TypeError:
+    st.error("Dados sobre as proporções de resíduos auxentes. Peça para o moderador fazer uma avaliação ou inserir os dados após a análise.")
+except psycopg2.Error as e:
+    st.error(f"Erro ao conectar no banco de dados.")
 
 
 # Função para exibir o formulário de coleta
